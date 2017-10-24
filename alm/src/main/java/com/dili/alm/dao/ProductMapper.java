a56754980e35f87be42b33169c92128a59f0127f
asiamaster <asiamastor@vip.qq.com>
2017-10-24 09:06:22 +0800

    更新模型，去掉无用代码

--- alm/src/main/java/com/dili/alm/dao/ProductMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dili.alm.dao;
-
-import com.dili.alm.domain.Product;
-import com.dili.ss.base.MyMapper;
-
-public interface ProductMapper extends MyMapper<Product> {
-}
--- alm/src/main/java/com/dili/alm/dao/SystemConfigMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dili.alm.dao;
-
-import com.dili.alm.domain.SystemConfig;
-import com.dili.ss.base.MyMapper;
-
-public interface SystemConfigMapper extends MyMapper<SystemConfig> {
-}
