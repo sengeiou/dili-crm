4e67ed2be3610ce16df748c26d1cde4848520029
DESKTOP-SQ9ASR4\jiang <jianglinhu@diligrp.com>
2017-11-01 18:00:28 +0800

    解决项目类型查询不到数据的问题

--- alm/src/main/java/com/dili/alm/cache/AlmCache.java
+++ alm/src/main/java/com/dili/alm/cache/AlmCache.java
@@ -1,17 +1,20 @@
 package com.dili.alm.cache;
 
-import com.dili.alm.domain.Project;
-import com.dili.alm.domain.User;
-
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import com.dili.alm.domain.Project;
+import com.dili.alm.domain.User;
 
 /**
  * Created by asiamaster on 2017/10/19 0019.
  */
 public class AlmCache {
-	//缓存用户，key为主键id
+	// 缓存用户，key为主键id
 	public static final Map<Long, User> userMap = new ConcurrentHashMap<>();
-	//缓存项目，key为主键id
+	// 缓存项目，key为主键id
 	public static final Map<Long, Project> projectMap = new ConcurrentHashMap<>();
+	// 缓存项目类型
+	public static final Map<String, String> projectTypeMap = new ConcurrentHashMap<>();
+
 }
--- alm/src/main/java/com/dili/alm/provider/ProjectTypeProvider.java
+++ alm/src/main/java/com/dili/alm/provider/ProjectTypeProvider.java
@@ -1,41 +1,63 @@
 package com.dili.alm.provider;
 
+import com.dili.alm.cache.AlmCache;
+import com.dili.alm.constant.AlmConstants;
+import com.dili.alm.domain.Project;
+import com.dili.alm.domain.dto.DataDictionaryValueDto;
+import com.dili.alm.service.ProjectService;
+import com.dili.ss.dto.DTOUtils;
 import com.dili.ss.metadata.FieldMeta;
 import com.dili.ss.metadata.ValuePair;
 import com.dili.ss.metadata.ValuePairImpl;
 import com.dili.ss.metadata.ValueProvider;
+import com.dili.sysadmin.sdk.session.SessionContext;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 /**
- * 由MyBatis Generator工具自动生成
- * This file was generated on 2017-10-24 14:31:10.
+ * 由MyBatis Generator工具自动生成 This file was generated on 2017-10-24 14:31:10.
  */
 @Component
-public class ProjectTypeProvider implements ValueProvider {
-    private static final List<ValuePair<?>> buffer;
+public class ProjectTypeProvider implements ValueProvider, ApplicationListener<ContextRefreshedEvent> {
 
-    static {
-        buffer = new ArrayList<ValuePair<?>>();
-        buffer.add(new ValuePairImpl("产品", "7"));
-        buffer.add(new ValuePairImpl("开发", "8"));
-    }
+	@Autowired
+	private ProjectService projectService;
 
-    @Override
-    public List<ValuePair<?>> getLookupList(Object obj, Map metaMap, FieldMeta fieldMeta) {
-        return buffer;
-    }
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		init();
+	}
 
-    @Override
-    public String getDisplayText(Object obj, Map metaMap, FieldMeta fieldMeta) {
-        if(obj == null || obj.equals("")) return null;
-        for(ValuePair<?> valuePair : buffer){
-            if(obj.toString().equals(valuePair.getValue())){
-                return valuePair.getText();
-            }
-        }
-        return null;
-    }
+	public void init() {
+		if (AlmCache.projectTypeMap.isEmpty()) {
+			List<DataDictionaryValueDto> list = projectService.getPojectTypes();
+			list.forEach(type -> {
+				AlmCache.projectTypeMap.put(type.getValue(), type.getCode());
+			});
+		}
+	}
+
+	@Override
+	public List<ValuePair<?>> getLookupList(Object obj, Map metaMap, FieldMeta fieldMeta) {
+		init();
+		List<ValuePair<?>> buffer = new ArrayList<ValuePair<?>>();
+		AlmCache.projectTypeMap.forEach((k, v) -> {
+			buffer.add(new ValuePairImpl<String>(v, k));
+		});
+		return buffer;
+	}
+
+	@Override
+	public String getDisplayText(Object obj, Map metaMap, FieldMeta fieldMeta) {
+		if (obj == null || obj.equals(""))
+			return null;
+		return AlmCache.projectTypeMap.get(obj.toString());
+	}
 }
