942cf9004868fe689aa4eea82ffe2bc6c26aac4f
asiamaster <asiamastor@vip.qq.com>
2017-11-03 09:07:16 +0800

    添加运维

--- alm/src/main/java/com/dili/alm/domain/TeamType.java
+++ alm/src/main/java/com/dili/alm/domain/TeamType.java
@@ -2,7 +2,7 @@ package com.dili.alm.domain;
 
 public enum TeamType {
 
-	PRODUCT("产品", 1), DEVELOP("开发", 2), TEST("测试", 3);
+	PRODUCT("产品", 1), DEVELOP("开发", 2), TEST("测试", 3), OPS("运维",4);
 
 	private String name;
 	private int code;
