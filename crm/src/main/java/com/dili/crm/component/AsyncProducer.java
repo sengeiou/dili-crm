8bd3d221b7ed7d5a17cb1774755f38a34f90b678
asiamaster <asiamastor@vip.qq.com>
2017-11-10 16:39:00 +0800

    初始导入!

---
+++ crm/src/main/java/com/dili/crm/component/AsyncProducer.java
@@ -0,0 +1,50 @@
+package com.dili.crm.component;
+
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendCallback;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+
+
+public class AsyncProducer {
+	public static void main(String[] args) throws MQClientException, InterruptedException, UnsupportedEncodingException {
+
+		DefaultMQProducer producer = new DefaultMQProducer("CID_JODIE_1");
+		producer.setNamesrvAddr("localhost:9876");
+		producer.setInstanceName("Producer");
+		producer.setVipChannelEnabled(false);
+		producer.setRetryTimesWhenSendAsyncFailed(0);
+		producer.start();
+		for (int i = 0; i < 1; i++) {
+			try {
+				final int index = i;
+				Message msg = new Message("Jodie_topic_1023",// topic
+						"TagA",// tag
+						"OrderID188",// key
+						("Hello MetaQ").getBytes(RemotingHelper.DEFAULT_CHARSET));// body
+				producer.send(msg, new SendCallback() {
+					@Override
+					public void onSuccess(SendResult sendResult) {
+						System.out.printf("%-10d OK %s %n", index, sendResult.getMsgId());
+					}
+
+					@Override
+					public void onException(Throwable e) {
+						System.out.printf("%-10d Exception %s %n", index, e);
+						e.printStackTrace();
+					}
+				});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+//            TimeUnit.MILLISECONDS.sleep(1);
+		}
+
+		producer.shutdown();
+	}
+}
---
+++ crm/src/main/java/com/dili/crm/component/TopicProducer.java
@@ -0,0 +1,31 @@
+package com.dili.crm.component;
+
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.remoting.common.RemotingHelper;
+import com.dili.ss.rocketmq.RocketMQProducer;
+import com.dili.ss.rocketmq.exception.RocketMqException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by asiamaster on 2017/11/7 0007.
+ */
+@Component
+public class TopicProducer {
+
+	@Autowired
+	private RocketMQProducer rocketMQProducer;
+
+	public void send(final String topic, final String tag, final String message){
+		try {
+			Message mesg = new Message(topic, tag, message.getBytes(RemotingHelper.DEFAULT_CHARSET));
+			rocketMQProducer.sendMsg(mesg);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}catch (RocketMqException e) {
+			System.out.println(String.format("发送MQ异常！TOPIC=%s,内容=%s", topic, message));
+		}
+	}
+}
