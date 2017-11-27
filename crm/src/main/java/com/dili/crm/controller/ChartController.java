eaf7d6b88774ea3d134e22be65d8b09f9f482769
wangguofeng <wangguofeng@diligrp.com>
2017-11-27 15:08:08 +0800

    新增客户回访记录报表

--- crm/src/main/java/com/dili/crm/controller/ChartController.java
+++ crm/src/main/java/com/dili/crm/controller/ChartController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSONObject;
 import com.dili.crm.domain.Address;
 import com.dili.crm.domain.dto.CustomerChartDTO;
+import com.dili.crm.domain.dto.CustomerVisitChartDTO;
 import com.dili.crm.service.CustomerService;
+import com.dili.crm.service.CustomerVisitService;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.metadata.ValueProviderUtils;
 import com.google.common.collect.Lists;
@@ -31,6 +33,7 @@ import io.swagger.annotations.ApiOperation;
 public class ChartController {
 	@Autowired
 	CustomerService customerService;
+	@Autowired CustomerVisitService customerVisitService;
 
 	@ApiOperation("跳转到图表页面")
 	@RequestMapping(value = "/index.html", method = RequestMethod.GET)
@@ -43,7 +46,11 @@ public class ChartController {
 	public String customerChart(ModelMap modelMap) {
 		return "chart/customer";
 	}
-
+	@ApiOperation("跳转到图表页面")
+	@RequestMapping(value = "/customerVisit.html", method = RequestMethod.GET)
+	public String customerVisitChart(ModelMap modelMap) {
+		return "chart/customerVisit";
+	}
 	@ApiOperation(value = "查询Address", notes = "查询Address，返回列表信息")
 	@ApiImplicitParams({
 			@ApiImplicitParam(name = "Address", paramType = "form", value = "Address的form信息", required = false, dataType = "string") })
@@ -86,6 +93,45 @@ public class ChartController {
 			}
 			
 	}
+	
+	@RequestMapping(value = "/customerVisitModeChart", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody Object customerVisitModeChart() {
+		 List<CustomerVisitChartDTO>data=this.customerVisitService.selectCustomerVisitGroupByMode().getData();
+			Map<Object, Object> metadata =this.getCustomerVisitMetadata();
+			try {
+				List<Map> list = ValueProviderUtils.buildDataByProvider(metadata, data);
+				return list;
+			} catch (Exception e) {
+				return Collections.emptyList();
+			}
+			
+	}
+	
+	
+	@RequestMapping(value = "/customerVisitStateChart", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody Object customerVisitStateChart() {
+		 	List<CustomerVisitChartDTO>data=this.customerVisitService.selectCustomerVisitGroupByState().getData();
+			Map<Object, Object> metadata =this.getCustomerVisitMetadata();
+			try {
+				List<Map> list = ValueProviderUtils.buildDataByProvider(metadata, data);
+				return list;
+			} catch (Exception e) {
+				return Collections.emptyList();
+			}
+			
+	}
+	
+	 private Map<Object, Object> getCustomerVisitMetadata(){
+	        Map<Object, Object> metadata = new HashMap<>();
+	        //回访状态
+	        JSONObject visitStateProvider = new JSONObject();
+	        metadata.put("state", visitStateProvider);
+	        //回访方式
+	        metadata.put("mode", getDDProvider(11L));
+	        return metadata;
+	    }
+
+	 
 	private Map<Object, Object> getCustomerMetadata(){
 		Map<Object, Object> metadata = new HashMap<>();
 
--- crm/src/main/java/com/dili/crm/dao/CustomerMapper.java
+++ crm/src/main/java/com/dili/crm/dao/CustomerMapper.java
@@ -3,10 +3,11 @@ package com.dili.crm.dao;
 import java.util.List;
 
 import com.dili.crm.domain.Customer;
+import com.dili.crm.domain.dto.CustomerChartDTO;
 import com.dili.ss.base.MyMapper;
 
 public interface CustomerMapper extends MyMapper<Customer> {
-	public List<Customer>selectCustomersGroupByType();
-	public List<Customer>selectCustomersGroupByMarket();
-	public List<Customer>selectCustomersGroupByProfession();
+	public List<CustomerChartDTO>selectCustomersGroupByType();
+	public List<CustomerChartDTO>selectCustomersGroupByMarket();
+	public List<CustomerChartDTO>selectCustomersGroupByProfession();
 }
