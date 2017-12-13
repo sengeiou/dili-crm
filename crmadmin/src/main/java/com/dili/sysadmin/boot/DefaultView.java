ec66f52c816341dc70ef84d62a3d7ccbdcb278b3
asiamaster <asiamastor@vip.qq.com>
2017-12-13 11:35:07 +0800

    修改域名和上下文路径

---
+++ crmadmin/src/main/java/com/dili/sysadmin/boot/DefaultView.java
@@ -0,0 +1,20 @@
+package com.dili.sysadmin.boot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by asiamaster on 2017/12/13 0013.
+ */
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/login/index.html");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		super.addViewControllers(registry);
+	}
+}
