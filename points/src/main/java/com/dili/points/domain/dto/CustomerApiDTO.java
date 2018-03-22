accbad8cc16a43b00c893418db8a64a56507d97d
wangmi <wangmi@diligrp.com>
2018-03-22 14:37:00 +0800

    客户远程批量提供者开发

---
+++ points/src/main/java/com/dili/points/domain/dto/CustomerApiDTO.java
@@ -0,0 +1,20 @@
+package com.dili.points.domain.dto;
+
+import com.dili.crm.domain.Customer;
+import com.dili.ss.domain.annotation.Operator;
+
+import javax.persistence.Column;
+import java.util.List;
+
+/**
+ * 客户接口的DTO，用于根据ids查询客户列表
+ * @author wangmi
+ */
+public interface CustomerApiDTO extends Customer {
+
+	@Operator(Operator.IN)
+	@Column(name = "`id`")
+	List<String> getIds();
+	void setIds(List<String> ids);
+
+}
