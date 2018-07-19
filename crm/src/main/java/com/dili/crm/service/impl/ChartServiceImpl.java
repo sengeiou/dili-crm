87b3efdb6cf341e94708a48f6f65bd820b3996b2
wangguofeng <wangguofeng@diligrp.com>
2018-07-19 14:25:11 +0800

    没有报表路径映射的市场将显示空白

--- crm/src/main/java/com/dili/crm/service/impl/ChartServiceImpl.java
+++ crm/src/main/java/com/dili/crm/service/impl/ChartServiceImpl.java
@@ -4,6 +4,7 @@ package com.dili.crm.service.impl;
 import java.text.MessageFormat;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -102,6 +103,9 @@ public class ChartServiceImpl implements ChartService{
 			if (output.getData() != null && output.getData().size() == 1) {
 				proxyValue= output.getData().get(0).getCode();
 			}
+		}
+		if(StringUtils.isBlank(proxyValue)) {
+			return "";
 		}
 		url= MessageFormat.format(url, new Object[]{proxyValue});
 		return url;
