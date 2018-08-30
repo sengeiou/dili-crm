14d8f7eaedcad7f659f305b494e5179506184496
wangmi <wangmi@diligrp.com>
2018-08-30 15:10:59 +0800

    新增兑换商品时对名称验重

--- points/src/main/java/com/dili/points/service/impl/ExchangeCommoditiesServiceImpl.java
+++ points/src/main/java/com/dili/points/service/impl/ExchangeCommoditiesServiceImpl.java
@@ -5,8 +5,10 @@ import com.dili.points.domain.ExchangeCommodities;
 import com.dili.points.service.ExchangeCommoditiesService;
 import com.dili.ss.base.BaseServiceImpl;
 import com.dili.ss.domain.BaseOutput;
+import com.dili.ss.dto.DTOUtils;
 import com.dili.uap.sdk.domain.UserTicket;
 import com.dili.uap.sdk.session.SessionContext;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -31,6 +33,19 @@ public class ExchangeCommoditiesServiceImpl extends BaseServiceImpl<ExchangeComm
         UserTicket userTicket = SessionContext.getSessionContext().getUserTicket();
         if(userTicket == null){
             return BaseOutput.failure("新增失败，登录超时");
+        }
+        if(StringUtils.isBlank(exchangeCommodities.getName())){
+            return BaseOutput.failure("新增失败，商品名称为空");
+        }
+        if(StringUtils.isBlank(exchangeCommodities.getFirmCode())){
+            return BaseOutput.failure("新增失败，市场编码为空");
+        }
+        ExchangeCommodities condition = DTOUtils.newDTO(ExchangeCommodities.class);
+        condition.setName(exchangeCommodities.getName());
+        condition.setFirmCode(exchangeCommodities.getFirmCode());
+        ExchangeCommodities exchangeCommodities1 = getActualDao().selectOne(condition);
+        if(exchangeCommodities1 != null){
+            return BaseOutput.failure("新增失败，商品名称重复");
         }
         //新增时，可兑换量即等于总量
         exchangeCommodities.setTotal(exchangeCommodities.getAvailable());
